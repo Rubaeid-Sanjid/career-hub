5bfa60d07495a3404ffc5e85e198515b4696654c
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-23 22:54:18 +0600

    route, tailwind, daisyUI setup and header footer root component added

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h3 className="text-5xl">Not found</h3>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+const Root = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Root;
+<Footer></Footer>;
