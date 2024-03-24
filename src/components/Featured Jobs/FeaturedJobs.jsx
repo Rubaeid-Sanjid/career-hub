17c2968321bfce1bff742de1ac28375a5e93061c
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-24 18:03:45 +0600

    added see all jobs button and finctionality

--- src/components/Featured Jobs/FeaturedJobs.jsx
+++ src/components/Featured Jobs/FeaturedJobs.jsx
@@ -3,13 +3,14 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [jobDataLength, setJobDataLength] = useState(4);
 
   useEffect(() => {
     fetch("jobs.json").then((res) => res.json().then((data) => setJobs(data)));
   }, []);
 
   return (
-    <div className="mb-10">
+    <div className="mb-12">
       <div>
         <h2 className="text-5xl text-center">Featured Jobs</h2>
         <p className="mt-5 text-center">
@@ -18,9 +19,14 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {
-            jobs.map(job => <Job key={job.id} job={job}></Job>)
-        }
+        {jobs.slice(0, jobDataLength).map((job) => (
+          <Job key={job.id} job={job}></Job>
+        ))}
+      </div>
+      <div className={`text-center mt-12 ${jobDataLength === jobs.length && 'hidden'}`}>
+        <button onClick={() => {setJobDataLength(jobs.length)}} className="btn bg-[#7E90FE] text-white text-xl">
+          See All Jobs
+        </button>
       </div>
     </div>
   );
